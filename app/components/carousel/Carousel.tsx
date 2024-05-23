ded73010d14ced41821832d3f507d7516d9c402b
Muhammad Azmi <azmi.filkom@gmail.com>
2024-05-23 23:22:54 +0700

    implement carousel component

---
+++ app/components/carousel/Carousel.tsx
@@ -0,0 +1,140 @@
+import { useCallback, useEffect, useRef } from "react";
+import {
+  EmblaCarouselType,
+  EmblaEventType,
+  EmblaOptionsType,
+} from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./CarouselArrowButton";
+import { DotButton, useDotButton } from "./CarouselDotButton";
+import styles from "./Carousel.module.scss";
+import clsx from "clsx";
+import Autoplay from "embla-carousel-autoplay";
+
+const TWEEN_FACTOR_BASE = 0.84;
+
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max);
+
+interface CarouselProps {
+  slides: {
+    image: string;
+    alt: string;
+  }[];
+  options?: EmblaOptionsType;
+}
+
+export default function Carousel(props: CarouselProps) {
+  const { slides, options } = props;
+  const plugins = [Autoplay()];
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, plugins);
+  const tweenFactor = useRef(0);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenOpacity = useCallback(
+    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+      const engine = emblaApi.internalEngine();
+      const scrollProgress = emblaApi.scrollProgress();
+      const slidesInView = emblaApi.slidesInView();
+      const isScrollEvent = eventName === "scroll";
+
+      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+        let diffToTarget = scrollSnap - scrollProgress;
+        const slidesInSnap = engine.slideRegistry[snapIndex];
+
+        slidesInSnap.forEach((slideIndex) => {
+          if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+          if (engine.options.loop) {
+            engine.slideLooper.loopPoints.forEach((loopItem) => {
+              const target = loopItem.target();
+
+              if (slideIndex === loopItem.index && target !== 0) {
+                const sign = Math.sign(target);
+
+                if (sign === -1) {
+                  diffToTarget = scrollSnap - (1 + scrollProgress);
+                }
+                if (sign === 1) {
+                  diffToTarget = scrollSnap + (1 - scrollProgress);
+                }
+              }
+            });
+          }
+
+          const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+          const opacity = numberWithinRange(tweenValue, 0, 1).toString();
+          emblaApi.slideNodes()[slideIndex].style.opacity = opacity;
+        });
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    setTweenFactor(emblaApi);
+    tweenOpacity(emblaApi);
+    emblaApi
+      .on("reInit", setTweenFactor)
+      .on("reInit", tweenOpacity)
+      .on("scroll", tweenOpacity)
+      .on("slideFocus", tweenOpacity);
+  }, [emblaApi, tweenOpacity]);
+
+  return (
+    <div className={styles["embla"]}>
+      <div className={styles["embla__viewport"]} ref={emblaRef}>
+        <div className={styles["embla__container"]}>
+          {slides.map((slide) => (
+            <div className={styles["embla__slide"]} key={slide.image}>
+              <img
+                className={styles["embla__slide__img"]}
+                src={`images/journey/${slide.image}`}
+                alt={slide.alt}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles["embla__controls"]}>
+        <div className={styles["embla__buttons"]}>
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className={styles["embla__dots"]}>
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={clsx([
+                styles["embla__dot"],
+                index === selectedIndex ? " embla__dot--selected" : "",
+              ])}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
