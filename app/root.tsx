5cfd0613efeb0dbf50aae4aaad28cbddc242c583
Muhammad Azmi <azmi.filkom@gmail.com>
2024-05-23 23:19:22 +0700

    add tailwind package, react icons, embla carousel

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,27 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import tailwindStylesheet from "~/tailwind.css?url";
+import globalStylesheet from "~/global.scss?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: tailwindStylesheet },
+  { rel: "stylesheet", href: globalStylesheet },
+  {
+    rel: "preconnect",
+    href: "https://fonts.googleapis.com",
+  },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap",
+  },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-primary text-on-primary josefin-sans-web">
         {children}
         <ScrollRestoration />
         <Scripts />
