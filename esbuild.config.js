516745f7e73a1385d8dedfd1cf55b513a4546151
Muhammad Azmi <azmi.filkom@gmail.com>
2024-05-27 23:36:35 +0700

    fix sass error on build

---
+++ esbuild.config.js
@@ -0,0 +1,13 @@
+const cssPlugin = require('./esbuild.css.plugin');
+const {sassPlugin} = require('esbuild-sass-plugin');
+
+require('esbuild')
+   .build({
+       entryPoints: ['dist/esm/index.js'],
+       bundle: true,
+       minify: true,
+       sourcemap: true,
+       outfile: 'dist/main/index.js',
+       plugins: [cssPlugin, sassPlugin()],
+   })
+   .catch(() => process.exit(1));
